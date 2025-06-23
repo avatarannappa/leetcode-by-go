d686c42c315d54335efe07e8f917ed8d08f01418
avatarannappa <nkcs.chenhao@gmail.com>
2025-06-23 10:35:49 +0800

    +add,678,有效的括号字符串,栈+贪心,star

---
+++ checkValidString678.go
@@ -0,0 +1,37 @@
+package main
+
+func checkValidString(s string) bool {
+	// 栈 + 贪心
+	var leftParentheses, asterisks []int
+	for i, c := range s {
+		if c == '(' {
+			leftParentheses = append(leftParentheses, i)
+		} else if c == '*' {
+			asterisks = append(asterisks, i)
+		} else {
+			if len(leftParentheses) > 0 {
+				leftParentheses = leftParentheses[:len(leftParentheses)-1]
+			} else if len(asterisks) > 0 {
+				asterisks = asterisks[:len(asterisks)-1]
+			} else {
+				return false
+			}
+		}
+	}
+
+	for len(leftParentheses) > 0 && len(asterisks) > 0 {
+		if leftParentheses[len(leftParentheses)-1] > asterisks[len(asterisks)-1] {
+			return false
+		}
+		leftParentheses = leftParentheses[:len(leftParentheses)-1]
+		asterisks = asterisks[:len(asterisks)-1]
+	}
+	return len(leftParentheses) == 0
+}
+
+func main() {
+	// Example usage
+	s := "(*))"
+	result := checkValidString(s)
+	println(result) // Output: true
+}
